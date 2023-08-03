f4a98b39a4ea0be03f0a861e6f2508da932bf537
Munjusjnr <munjusjnr54@outlook.com>
2023-08-03 08:58:21 -0700

    update on files

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+#include <limits.h>
+
+/**
+ * binary_search_min_max_value - A helper function that searches for the
+ * minimum maximum value through the binary tree
+ * @tree: A pointer to the root of the binary tree
+ * Return: maximum value in the search
+ */
+
+int binary_search_min_max_value(const binary_tree_t *tree, int min, int max)
+{
+	if (tree != NULL)
+	{
+		if (tree->n < min || tree->n > max)
+			 return (0);
+		return (binary_search_min_max_value(tree->left, min,
+			tree->n - 1) && binary_search_min_max_value
+			(tree->right, tree->n + 1, max));
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_bst - A function that checks if a binary tree is a valid
+ * binary search tree
+ * @tree: A pointer to the root of the binary tree
+ * Return: 1 if successful 0 if not
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_search_min_max_value(tree, INT_MIN, INT_MAX));
+}
